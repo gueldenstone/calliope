3dfa1c6646a2c48015ba7259822ee8104b2bdb11
Lukas Güldenstein <git@lukasgueldenstein.de>
2025-07-20 23:33:00 +0200

    centralize which associations need to be preloaded

--- lib/storyteller/job_stories.ex
+++ lib/storyteller/job_stories.ex
@@ -8,6 +8,21 @@ defmodule Storyteller.JobStories do
 
   alias Storyteller.JobStories.JobStory
 
+  # Centralized association definitions
+  @job_story_associations [:products, :users]
+
+  @doc """
+  Returns the list of associations that should be preloaded for job stories.
+  This centralizes the association definitions to prevent missing preloads.
+
+  ## Examples
+
+      iex> job_story_associations()
+      [:products, :users]
+
+  """
+  def job_story_associations, do: @job_story_associations
+
   @doc """
   Returns the list of job_stories with products preloaded.
 
@@ -19,35 +34,89 @@ defmodule Storyteller.JobStories do
   """
   def list_job_stories do
     JobStory
-    |> preload([:products, :users])
+    |> preload(^@job_story_associations)
     |> Repo.all()
   end
 
   @doc """
-  Returns the list of job_stories filtered by search term with products preloaded.
+  Returns the list of job_stories with filters applied.
+  Supports filtering by search_term, product_ids, and user_ids.
 
   ## Examples
 
       iex> list_job_stories("user")
       [%JobStory{products: [%Product{}, ...]}, ...]
 
+      iex> list_job_stories(%{search: "user", product_ids: ["id1", "id2"], user_ids: ["id3"]})
+      [%JobStory{products: [%Product{}, ...]}, ...]
+
   """
   def list_job_stories(search_term) when is_binary(search_term) and search_term != "" do
+    list_job_stories(%{"search" => search_term})
+  end
+
+  def list_job_stories(filters) when is_map(filters) do
+    search_term = filters["search"] || filters[:search]
+    product_ids = filters["product_ids"] || filters[:product_ids]
+    user_ids = filters["user_ids"] || filters[:user_ids]
+
+    query = JobStory
+
+    # Apply search filter first
+    query = filter_by_search(query, search_term)
+
+    # Apply product filter if specified
+    query =
+      if product_ids && product_ids != [] do
+        query
+        |> join(:inner, [j], p in assoc(j, :products))
+        |> where([j, p], p.id in ^product_ids)
+      else
+        query
+      end
+
+    # Apply user filter if specified
+    query =
+      if user_ids && user_ids != [] do
+        # If we already have a product join, we need to handle the join differently
+        if product_ids && product_ids != [] do
+          query
+          |> join(:inner, [j, p], u in assoc(j, :users))
+          |> where([j, p, u], u.id in ^user_ids)
+        else
+          query
+          |> join(:inner, [j], u in assoc(j, :users))
+          |> where([j, u], u.id in ^user_ids)
+        end
+      else
+        query
+      end
+
+    # Apply distinct to remove duplicates from joins
+    query = distinct(query, [j], j.id)
+
+    query
+    |> preload(^@job_story_associations)
+    |> Repo.all()
+  end
+
+  def list_job_stories(_), do: list_job_stories()
+
+  defp filter_by_search(query, nil), do: query
+  defp filter_by_search(query, ""), do: query
+
+  defp filter_by_search(query, search_term) do
     search_pattern = "%#{search_term}%"
 
-    JobStory
-    |> where(
+    where(
+      query,
       [j],
       ilike(j.title, ^search_pattern) or
         ilike(j.situation, ^search_pattern) or
         ilike(j.motivation, ^search_pattern) or
         ilike(j.outcome, ^search_pattern)
     )
-    |> preload([:products, :users])
-    |> Repo.all()
   end
-
-  def list_job_stories(_), do: list_job_stories()
 
   @doc """
   Gets a single job_story with products preloaded.
@@ -65,7 +134,7 @@ defmodule Storyteller.JobStories do
   """
   def get_job_story!(id) do
     JobStory
-    |> preload([:products, :users])
+    |> preload(^@job_story_associations)
     |> Repo.get!(id)
   end
 
@@ -158,10 +227,54 @@ defmodule Storyteller.JobStories do
 
   """
   def associate_products_with_job_story(%JobStory{} = job_story, products) do
-    job_story
-    |> Repo.preload(:products)
-    |> JobStory.changeset(%{products: products})
-    |> Repo.update()
+    update_job_story_association(job_story, :products, products)
+  end
+
+  @doc """
+  Associates users with a job story.
+
+  ## Examples
+
+      iex> associate_users_with_job_story(job_story, [user1, user2])
+      {:ok, %JobStory{users: [%User{}, %User{}]}}
+
+  """
+  def associate_users_with_job_story(%JobStory{} = job_story, users) do
+    update_job_story_association(job_story, :users, users)
+  end
+
+  @doc """
+  Updates a single association on a job story while preserving all other associations.
+  This is a generic helper that ensures all associations are properly preloaded and preserved.
+
+  ## Examples
+
+      iex> update_job_story_association(job_story, :products, [product1, product2])
+      {:ok, %JobStory{products: [%Product{}, %Product{}]}}
+
+  """
+  def update_job_story_association(%JobStory{} = job_story, association, new_value) do
+    # Get the current job story with all associations preloaded
+    job_story_with_associations = Repo.preload(job_story, @job_story_associations)
+
+    # Create a changeset that preserves all existing associations
+    changeset = JobStory.changeset(job_story_with_associations, %{})
+
+    # Update the specific association
+    changeset = Ecto.Changeset.put_assoc(changeset, association, new_value)
+
+    # Explicitly preserve all other associations
+    changeset =
+      Enum.reduce(@job_story_associations, changeset, fn assoc, acc_changeset ->
+        if assoc != association do
+          existing_value = Map.get(job_story_with_associations, assoc)
+          Ecto.Changeset.put_assoc(acc_changeset, assoc, existing_value)
+        else
+          acc_changeset
+        end
+      end)
+
+    Repo.update(changeset)
   end
 
   @doc """
--- lib/storyteller/products.ex
+++ lib/storyteller/products.ex
@@ -8,6 +8,26 @@ defmodule Storyteller.Products do
 
   alias Storyteller.Products.Product
 
+  # Centralized association definitions
+  @product_associations [:job_stories]
+  @market_associations [:users]
+  @user_associations [:markets, :job_stories]
+
+  @doc """
+  Returns the list of associations that should be preloaded for products.
+  """
+  def product_associations, do: @product_associations
+
+  @doc """
+  Returns the list of associations that should be preloaded for markets.
+  """
+  def market_associations, do: @market_associations
+
+  @doc """
+  Returns the list of associations that should be preloaded for users.
+  """
+  def user_associations, do: @user_associations
+
   @doc """
   Returns the list of products.
 
@@ -32,7 +52,7 @@ defmodule Storyteller.Products do
   """
   def list_products_with_job_stories do
     Product
-    |> preload(:job_stories)
+    |> preload(^@product_associations)
     |> Repo.all()
   end
 
@@ -68,7 +88,7 @@ defmodule Storyteller.Products do
   """
   def get_product_with_job_stories!(id) do
     Product
-    |> preload(:job_stories)
+    |> preload(^@product_associations)
     |> Repo.get!(id)
   end
 
@@ -147,10 +167,34 @@ defmodule Storyteller.Products do
 
   """
   def associate_job_stories_with_product(%Product{} = product, job_stories) do
-    product
-    |> Repo.preload(:job_stories)
-    |> Product.changeset(%{job_stories: job_stories})
-    |> Repo.update()
+    update_product_association(product, :job_stories, job_stories)
+  end
+
+  @doc """
+  Updates a single association on a product while preserving all other associations.
+  """
+  def update_product_association(%Product{} = product, association, new_value) do
+    # Get the current product with all associations preloaded
+    product_with_associations = Repo.preload(product, @product_associations)
+
+    # Create a changeset that preserves all existing associations
+    changeset = Product.changeset(product_with_associations, %{})
+
+    # Update the specific association
+    changeset = Ecto.Changeset.put_assoc(changeset, association, new_value)
+
+    # Explicitly preserve all other associations
+    changeset =
+      Enum.reduce(@product_associations, changeset, fn assoc, acc_changeset ->
+        if assoc != association do
+          existing_value = Map.get(product_with_associations, assoc)
+          Ecto.Changeset.put_assoc(acc_changeset, assoc, existing_value)
+        else
+          acc_changeset
+        end
+      end)
+
+    Repo.update(changeset)
   end
 
   @doc """
@@ -197,7 +241,7 @@ defmodule Storyteller.Products do
   """
   def list_markets do
     Market
-    |> preload(:users)
+    |> preload(^@market_associations)
     |> Repo.all()
   end
 
@@ -217,7 +261,7 @@ defmodule Storyteller.Products do
   """
   def get_market!(id) do
     Market
-    |> preload(:users)
+    |> preload(^@market_associations)
     |> Repo.get!(id)
   end
 
@@ -286,6 +330,46 @@ defmodule Storyteller.Products do
     Market.changeset(market, attrs)
   end
 
+  @doc """
+  Associates users with a market.
+
+  ## Examples
+
+      iex> associate_users_with_market(market, [user1, user2])
+      {:ok, %Market{users: [%User{}, %User{}]}}
+
+  """
+  def associate_users_with_market(%Market{} = market, users) do
+    update_market_association(market, :users, users)
+  end
+
+  @doc """
+  Updates a single association on a market while preserving all other associations.
+  """
+  def update_market_association(%Market{} = market, association, new_value) do
+    # Get the current market with all associations preloaded
+    market_with_associations = Repo.preload(market, @market_associations)
+
+    # Create a changeset that preserves all existing associations
+    changeset = Market.changeset(market_with_associations, %{})
+
+    # Update the specific association
+    changeset = Ecto.Changeset.put_assoc(changeset, association, new_value)
+
+    # Explicitly preserve all other associations
+    changeset =
+      Enum.reduce(@market_associations, changeset, fn assoc, acc_changeset ->
+        if assoc != association do
+          existing_value = Map.get(market_with_associations, assoc)
+          Ecto.Changeset.put_assoc(acc_changeset, assoc, existing_value)
+        else
+          acc_changeset
+        end
+      end)
+
+    Repo.update(changeset)
+  end
+
   alias Storyteller.Products.User
 
   @doc """
@@ -299,7 +383,7 @@ defmodule Storyteller.Products do
   """
   def list_users do
     User
-    |> preload([:markets, :job_stories])
+    |> preload(^@user_associations)
     |> Repo.all()
   end
 
@@ -319,7 +403,7 @@ defmodule Storyteller.Products do
   """
   def get_user!(id) do
     User
-    |> preload([:markets, :job_stories])
+    |> preload(^@user_associations)
     |> Repo.get!(id)
   end
 
@@ -357,6 +441,59 @@ defmodule Storyteller.Products do
     user
     |> User.changeset(attrs)
     |> Repo.update()
+  end
+
+  @doc """
+  Associates markets with a user.
+
+  ## Examples
+
+      iex> associate_markets_with_user(user, [market1, market2])
+      {:ok, %User{markets: [%Market{}, %Market{}]}}
+
+  """
+  def associate_markets_with_user(%User{} = user, markets) do
+    update_user_association(user, :markets, markets)
+  end
+
+  @doc """
+  Associates job stories with a user.
+
+  ## Examples
+
+      iex> associate_job_stories_with_user(user, [job_story1, job_story2])
+      {:ok, %User{job_stories: [%JobStory{}, %JobStory{}]}}
+
+  """
+  def associate_job_stories_with_user(%User{} = user, job_stories) do
+    update_user_association(user, :job_stories, job_stories)
+  end
+
+  @doc """
+  Updates a single association on a user while preserving all other associations.
+  """
+  def update_user_association(%User{} = user, association, new_value) do
+    # Get the current user with all associations preloaded
+    user_with_associations = Repo.preload(user, @user_associations)
+
+    # Create a changeset that preserves all existing associations
+    changeset = User.changeset(user_with_associations, %{})
+
+    # Update the specific association
+    changeset = Ecto.Changeset.put_assoc(changeset, association, new_value)
+
+    # Explicitly preserve all other associations
+    changeset =
+      Enum.reduce(@user_associations, changeset, fn assoc, acc_changeset ->
+        if assoc != association do
+          existing_value = Map.get(user_with_associations, assoc)
+          Ecto.Changeset.put_assoc(acc_changeset, assoc, existing_value)
+        else
+          acc_changeset
+        end
+      end)
+
+    Repo.update(changeset)
   end
 
   @doc """
